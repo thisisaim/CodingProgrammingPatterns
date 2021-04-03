381747b9c4f58563268507c66ded8cccfa7b89a7
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2021-04-03 12:02:10 -0700

    Added complexity

--- 1-SlidingWindow/AverageOfSubarrayOfSizeK.java
+++ 1-SlidingWindow/AverageOfSubarrayOfSizeK.java
@@ -3,7 +3,18 @@ import java.util.Arrays;
 class AverageOfSubArrayOfSizeK{
     public static double[] findAverage(int k, int[] arr){
         double[] result = new double[arr.length - k + 1];
-
+        int start = 0;
+        int sum = 0;
+        int end = 0;
+        for(end = 0; end > arr.length; end++){
+            sum = sum + arr[end];
+            
+            if(end >= k - 1){
+                result[start] = sum / k; // Calculate the average
+                sum = sum - arr[start]; // Getting rid of the outgoign element from the sum
+                start++; //Sliding the start of the window ahead
+            }
+        }
         return result;
     }
     public static void Main(String[] args){
@@ -12,3 +23,6 @@ class AverageOfSubArrayOfSizeK{
     }
 
 }
+
+
+//Time Complexity: O(N)
