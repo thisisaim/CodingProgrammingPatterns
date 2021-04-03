8112888b277dfd3846ec1d2777dbc983ffa22542
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2021-04-03 11:52:17 -0700

    First Commit

---
+++ 1-SlidingWindow/AverageOfSubarrayOfSizeK.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class AverageOfSubArrayOfSizeK{
+    public static double[] findAverage(int k, int[] arr){
+        double[] result = new double[arr.length - K + 1];
+
+        return result;
+    }
+    public static void Main(String[] args){
+        double[] result = AverageOfSubArrayOfSizeK.findAverage(5, new int[] {1, 2, 3, 4, 5, 6, 7, 8});
+        System.out.println((Arrays.toString(result)));
+    }
+
+}
