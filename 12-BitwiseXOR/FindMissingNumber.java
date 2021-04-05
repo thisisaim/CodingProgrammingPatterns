afc9b3cf2a39a0a6adc3062d40e4ddf18eea478b
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-04-05 11:16:29 -0700

    First Message

---
+++ 12-BitwiseXOR/FindMissingNumber.java
@@ -0,0 +1,24 @@
+class MissingNumber{
+    public static int FindMissingNumber(int[] arr){
+        int length = arr.length + 1;
+        
+        int x1 = 1;
+
+        for(int i = 2; i < length; i++){
+            x1 = x1 ^ i;
+        }
+
+        int x2 = arr[0];
+        for(int i = 1; i < length - 1; i++){
+            x2 = x2 ^ i;
+        }
+        return x1 ^ x2;
+    }
+
+    //Time Complexity O(N)
+    //Space Complexity O(1)
+    
+    public static void main(String[] args) {
+        int[] arr = new int{1, 5, 2, 6, 4};
+    }
+}
