f53ceb7e89401a5eb3822e57a08cb5e0a76a4e90
AVerapornpongkul <AVerapornpongkul@MCIT-013972>
2021-04-21 11:35:14 -0700

    First Commit

---
+++ 1-SlidingWindow/CharacterReplacement.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class CharacterReplacement{
+    public static int findLength(String str, int k) {
+        Map<Character, Integer> freqMap = new HashMap<>();
+
+        int windowStart = 0;
+        int maxLength = 0;
+        int maxRepeatLetterCount = 0;
+
+        for(int windowEnd = 0; windowEnd < str.length(); windowEnd++){
+            char rightChar = str.charAt(windowEnd);
+            freqMap.put(rightChar, freqMap.getOrDefault(rightChar, 0) + 1);
+            maxRepeatLetterCount = Math.max(maxRepeatLetterCount, freqMap.get(rightChar));
+            
+            if(windowEnd - windowStart + 1 - maxRepeatLetterCount > k){
+                char leftChar = str.charAt(windowStart);
+                freqMap.put(leftChar, freqMap.getOrDefault(leftChar) - 1);
+                windowStart++;
+            }
+            maxLength = Math.max(maxLength, windowEnd - windowStart + 1);
+        }
+        return maxLength;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(CharacterReplacement.findLength("aabccbb", 2));
+        System.out.println(CharacterReplacement.findLength("abbcb", 1));
+        System.out.println(CharacterReplacement.findLength("abccde", 1));
+    }
+}
