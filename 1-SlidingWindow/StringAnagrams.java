f04e232c4dae2214ce6d3ceab9a7f1b2c8287c11
Aim Verapornpongkul <aimverapornpongkul@gmail.com>
2021-04-23 00:13:07 -0700

    First Commit

---
+++ 1-SlidingWindow/StringAnagrams.java
@@ -0,0 +1,71 @@
+/*
+Given a string and a pattern, find all anagrams of the pattern in the given string.
+
+Anagram is actually a Permutation of a string. For example, “abc” has the following six anagrams:
+
+abc
+acb
+bac
+bca
+cab
+cba
+Write a function to return a list of starting indices of the anagrams of the pattern in the given string.
+
+Input: String="ppqp", Pattern="pq"
+Output: [1, 2]
+Explanation: The two anagrams of the pattern in the given string are "pq" and "qp".
+
+
+Input: String="abbcabc", Pattern="abc"
+Output: [2, 3, 4]
+Explanation: The three anagrams of the pattern in the given string are "bca", "cab", and "abc".
+
+*/
+
+
+import java.util.*;
+
+class StringAnagrams{
+    public static List<Integer> findStringAnagrams(String str, String pattern) {
+        int windowStart = 0;
+        int matched = 0;
+        
+        Map<Character, Integer> freqMap = new HashMap<>();
+
+        for(char c : pattern.toCharArray()){
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+
+        List<Integer> result = new ArrayList<Integer>();
+
+        for(int windowEnd = 0; windowEnd < str.length(); windowEnd++){
+            char rightChar = str.charAt(windowEnd);
+
+            if(freqMap.containsKey(rightChar)){
+                freqMap.put(rightChar, freqMap.get(rightChar) - 1);
+                if(freqMap.get(rightChar) == 0){
+                    matched++;
+                }
+            }
+            if(matched == freqMap.size()){
+                result.add(windowStart);
+            }
+            
+            if (windowEnd >= pattern.length() - 1) { // shrink the window
+            char leftChar = str.charAt(windowStart++);
+            if (charFrequencyMap.containsKey(leftChar)) {
+                if (charFrequencyMap.get(leftChar) == 0)
+                    matched--; // before putting the character back, decrement the matched count
+                    // put the character back
+                    charFrequencyMap.put(leftChar, charFrequencyMap.get(leftChar) + 1);
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StringAnagrams.findStringAnagrams("ppqp", "pq"));
+        System.out.println(StringAnagrams.findStringAnagrams("abbcabc", "abc"));
+    }
+}
