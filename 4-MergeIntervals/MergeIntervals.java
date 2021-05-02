ea7b39c407fd56eace39873e50aae601c7bf446f
Aim Verapornpongkul <AimVerapornpongkul@gmail.com>
2021-05-01 21:40:26 -0700

    Added overlaod method for 2d array medthod signature

--- 4-MergeIntervals/MergeIntervals.java
+++ 4-MergeIntervals/MergeIntervals.java
@@ -43,6 +43,29 @@ class MergeIntervals {
     return mergedIntervals;
   }
 
+  public int[][] merge(int[][] intervals){
+    List<int[]> result = new ArrayList<>();
+    if(intervals.length <= 1){
+      return intervals;
+    }
+    //Sort by starting time, befoe processing
+    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+    int[] newInterval = intervals[0];
+    result.add(newInterval);
+
+    for(int[] interval : intervals){
+      if(interval[0] <= newInterval[1]){
+        newInterval[1] = Math.max(interval[1], newInterval[1]);
+      }
+      else{
+        newInterval = interval;
+        result.add(newInterval);
+      }
+    }
+    return result.toArray(new int[result.size()][]);
+  }
+
   //Time O(N LogN)
   //Space O(N)
 
