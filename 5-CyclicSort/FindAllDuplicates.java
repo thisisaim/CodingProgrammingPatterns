968d05b2d7312b1f1ef8b38108dc79e3f028b2e0
Aim Verapornpongkul <AimVerapornpongkul@gmail.com>
2021-05-04 14:54:21 -0700

    Mispelled filename and class name

--- 5-CyclicSort/FindAllDuplicates.java
+++ 5-CyclicSort/FindAllDuplicates.java
@@ -1,9 +1,11 @@
 import java.util.*;
 
-class FindAllDupliates{
+class FindAllDuplicates{
     public static List<Integer> findNumbers(int[] nums) {
         int i = 0;
         List<Integer> result = new ArrayList<>();
+
+        //Putting each number at the correct index.
         while(i < nums.length){
             if(nums[i] != nums[nums[i] - 1]){
                 swap(nums, i, nums[i] - 1);
@@ -13,6 +15,7 @@ class FindAllDupliates{
             }
         }
 
+        //Iterate through the array to find the numbers t
         for(i = 0; i < nums.length; i++){
             if(nums[i] != i + 1){
                 result.add(nums[i]);
@@ -29,9 +32,14 @@ class FindAllDupliates{
 
     public static void main(String[] args) {
         int[] test = new int[] { 3, 4, 4, 5, 5 };
-        System.out.println("Duplicates are: " + FindAllDupliates.findNumbers(test));
+        System.out.println("Duplicates are: " + FindAllDuplicates.findNumbers(test));
 
         test = new int[] { 5, 4, 7, 2, 3, 5, 3 };
-        System.out.println("Duplicates are: " + FindAllDupliates.findNumbers(test));
+        System.out.println("Duplicates are: " + FindAllDuplicates.findNumbers(test));
+
+        //Time O(n)
+        //Space O(1)
+
+
     }
 }
