229fd0d493024951894110b33af3d8106517c7a4
ibaim <thisisaim@gmail.com>
2021-05-04 23:32:17 -0700

    changed to take the code patterns

--- 1-SlidingWindow/AverageOfSubarrayOfSizeK.java
+++ 1-SlidingWindow/AverageOfSubarrayOfSizeK.java
@@ -1,28 +1,24 @@
-import java.util.*;
+import java.util.Arrays;
 
-class AverageOfSubArrayOfSizeK{
-    public static double[] findAverage(int k, int[] arr){
-        double[] result = new double[arr.length - k + 1];
-        int start = 0;
-        int sum = 0;
-        int end = 0;
-        for(end = 0; end > arr.length; end++){
-            sum = sum + arr[end];
-            
-            if(end >= k - 1){
-                result[start] = sum / k; // Calculate the average
-                sum = sum - arr[start]; // Getting rid of the outgoign element from the sum
-                start++; //Sliding the start of the window ahead
-            }
-        }
-        return result;
+class AverageOfSubarrayOfSizeK {
+  public static double[] findAverages(int K, int[] arr) {
+    double[] result = new double[arr.length - K + 1];
+    double windowSum = 0;
+    int windowStart = 0;
+    for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
+      windowSum += arr[windowEnd]; // add the next element
+      // slide the window, we don't need to slide if we've not hit the required window size of 'k'
+      if (windowEnd >= K - 1) {
+        result[windowStart] = windowSum / K; // calculate the average
+        windowSum -= arr[windowStart]; // subtract the element going out
+        windowStart++; // slide the window ahead
+      }
     }
-    public static void Main(String[] args){
-        double[] result = AverageOfSubArrayOfSizeK.findAverage(5, new int[] {1, 2, 3, 4, 5, 6, 7, 8});
-        System.out.println((Arrays.toString(result)));
-    }
+    return result;
+  }
 
+  public static void main(String[] args) {
+    double[] result = AverageOfSubarrayOfSizeK.findAverages(8, new int[] { 1, 3, 2, 6, -1, 4, 1, 8, 2 });
+    System.out.println("Averages of subarrays of size K: " + Arrays.toString(result));
+  }
 }
-
-
-//Time Complexity: O(N)
