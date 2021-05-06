5fb5c2624f9526f053dbda56984fd3ccada3eee8
Aim Verapornpongkul <AimVerapornpongkul@gmail.com>
2021-05-06 15:55:25 -0700

    spacing

--- 7-BFS/ReverseLevelOrderTraversal.java
+++ 7-BFS/ReverseLevelOrderTraversal.java
@@ -1,51 +1,56 @@
 import java.util.*;
 
-class TreeNode {
-  int val;
-  TreeNode left;
-  TreeNode right;
+class TreeNode{
+    int val;
+    TreeNode left;
+    TreeNode right;
 
-  TreeNode(int x) {
-    val = x;
-  }
+    TreeNode(int x){
+        val = x;
+    }
 };
 
-class ReverseLevelOrderTraversal {
-  public static List<List<Integer>> traverse(TreeNode root) {
-    List<List<Integer>> result = new LinkedList<List<Integer>>();
-    if (root == null)
-      return result;
+class ReverseLevelOrderTraversal{
+    public static List<List<Integer>> traverse(TreeNode root){
+        List<List<Integer>> result = new LinkedList<List<Integer>>();
 
-    Queue<TreeNode> queue = new LinkedList<>();
-    queue.offer(root);
-    while (!queue.isEmpty()) {
-      int levelSize = queue.size();
-      List<Integer> currentLevel = new ArrayList<>(levelSize);
-      for (int i = 0; i < levelSize; i++) {
-        TreeNode currentNode = queue.poll();
-        // add the node to the current level
-        currentLevel.add(currentNode.val);
-        // insert the children of current node to the queue
-        if (currentNode.left != null)
-          queue.offer(currentNode.left);
-        if (currentNode.right != null)
-          queue.offer(currentNode.right);
-      }
-      // append the current level at the beginning
-      result.add(0, currentLevel);
+        if(root == null){
+            return result;
+        }
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int levelSize = q.size();
+            List<Integer> currentLevel = new ArrayList<>(levelSize);
+
+            for(int i = 0; i < levelSize; i++){
+                TreeNode currentNode = q.poll();
+                
+                currentLevel.add(currentNode.val);
+                
+                if(currentNode.left != null){
+                    q.offer(currentNode.left);
+                }
+
+                if(currentNode.right != null){
+                    q.offer(currentNode.right);
+                }
+            }
+            result.add(0, currentLevel);
+
+        }
+        return result;
     }
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(12);
+        root.left = new TreeNode(7);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(9);
+        root.right.left = new TreeNode(10);
+        root.right.right = new TreeNode(5);
+        List<List<Integer>> result = ReverseLevelOrderTraversal.traverse(root);
+        System.out.println("Reverse level order traversal: " + result);
+      }
 
-    return result;
-  }
-
-  public static void main(String[] args) {
-    TreeNode root = new TreeNode(12);
-    root.left = new TreeNode(7);
-    root.right = new TreeNode(1);
-    root.left.left = new TreeNode(9);
-    root.right.left = new TreeNode(10);
-    root.right.right = new TreeNode(5);
-    List<List<Integer>> result = ReverseLevelOrderTraversal.traverse(root);
-    System.out.println("Reverse level order traversal: " + result);
-  }
 }
