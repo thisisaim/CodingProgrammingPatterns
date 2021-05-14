e79e924e0b37b4f148793584606ec136dbeb5517
Aim Verapornpongkul <AimVerapornpongkul@gmail.com>
2021-05-14 10:49:41 -0700

    Shorter more generic solution for permutation problems

---
+++ 9-TwoHeaps/Permutations.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class Permutations {
+
+    public static List<List<Integer>> findPermutations(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        // Arrays.sort(nums); // not necessary
+        backtrack(list, new ArrayList<>(), nums);
+        return list;
+     }
+     
+     private static void backtrack(List<List<Integer>> list, List<Integer> tempList, int [] nums){
+        if(tempList.size() == nums.length){
+           list.add(new ArrayList<>(tempList));
+        } else{
+           for(int i = 0; i < nums.length; i++){ 
+              if(tempList.contains(nums[i])) continue; // element already exists, skip
+              tempList.add(nums[i]);
+              backtrack(list, tempList, nums);
+              tempList.remove(tempList.size() - 1);
+           }
+        }
+    }
+
+  public static void main(String[] args) {
+    List<List<Integer>> result = Permutations.findPermutations(new int[] { 1, 3, 5 });
+    System.out.print("Here are all the permutations: " + result);
+  }
+}
