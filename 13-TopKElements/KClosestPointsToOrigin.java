e03ded3526a08648bf874b67f295d90f0dfdca81
Aim Verapornpongkul <AimVerapornpongkul@gmail.com>
2021-05-17 15:40:19 -0700

    First commit

---
+++ 13-TopKElements/KClosestPointsToOrigin.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class Point{
+    int x;
+    int y;
+
+    public Point(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int distFromOrigin() {
+        return (x * x) + (y * y);
+    }
+}
+
+class KClosestPointsToOrigin{
+    public static List<Point> findClosestPoints(Point[] points, int k) {
+        PriorityQueue<Point> maxHeap = new PriorityQueue<>((a, b) ->b.distFromOrigin() - a.distFromOrigin());
+
+        for(int i = 0; i < k; i++){
+            maxHeap.add(points[i]);
+        }
+
+        for(int i = k; i < points.length; i++){
+            if(points[i].distFromOrigin() < maxHeap.peek().distFromOrigin()){
+                maxHeap.poll();
+                maxHeap.add(points[i]);
+            }
+        }
+        return new ArrayList<>(maxHeap);
+    }
+
+
+    public static void main(String[] args) {
+        Point[] points = new Point[] { new Point(1, 3), new Point(3, 4), new Point(2, -1) };
+        List<Point> result = KClosestPointsToOrigin.findClosestPoints(points, 2);
+        System.out.print("Here are the k points closest the origin: ");
+        for (Point p : result)
+          System.out.print("[" + p.x + " , " + p.y + "] ");
+      }
+}
