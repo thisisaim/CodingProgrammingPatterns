50e8a1d4eb1df3fba319f2830bf7048f47283f9f
Aim Verapornpongkul <AimVerapornpongkul@gmail.com>
2021-05-17 11:14:35 -0700

    modified main for multiple test cases

--- 11-ModifiedBinarySearch/OrderAgnosticBinarySearch.java
+++ 11-ModifiedBinarySearch/OrderAgnosticBinarySearch.java
@@ -37,6 +37,9 @@ class BinarySearch{
     //Space Complexity O(1)
     
     public static void main(String[] args){
-        System.out.println(BinarySearch.search(new int[] {1, 2, 3, 4, 5, 4}));
+        System.out.println(BinarySearch.search(new int[] { 4, 6, 10 }, 10));
+        System.out.println(BinarySearch.search(new int[] { 1, 2, 3, 4, 5, 6, 7 }, 5));
+        System.out.println(BinarySearch.search(new int[] { 10, 6, 4 }, 10));
+        System.out.println(BinarySearch.search(new int[] { 10, 6, 4 }, 4));
     }
 }
