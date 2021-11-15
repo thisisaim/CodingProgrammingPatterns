eb7b7824191e1036029fa88c53710fef657eeaa7
ibaim <thisisaim@gmail.com>
2021-11-14 20:11:55 -0800

    bug fixed

--- 2-TwoPointers/DutchFlag.java
+++ 2-TwoPointers/DutchFlag.java
@@ -1,23 +1,26 @@
 class DutchFlag{
-    public static void sort(int[] arr){
-        int left = 0;
-        int right = arr.length - 1;
-
-        for(int i = 0; i < arr.length; i++){
-            if(arr[i] == 0){
-                swap(arr, i, left);
-                i++;
-                left++;
-            }
-            else if (arr[i] == 1){
-                i++;
-            }
-            else{
-                swap(arr, i, right);
-                right--;
-            }
+    public static void sort(int[] arr) {
+    // all elements < low are 0 and all elements > high are 2
+    // all elements from >= low < i are 1
+    int low = 0;
+    int high = arr.length - 1;
+    for (int i = 0; i <= high;) {
+        if (arr[i] == 0) {
+            swap(arr, i, low);
+            // increment 'i' and 'low'
+            i++;
+            low++;
+        } 
+        else if (arr[i] == 1) {
+            i++;
+        } 
+        else { // the case for arr[i] == 2
+            swap(arr, i, high);
+            // decrement 'high' only, after the swap the number at index 'i' could be 0, 1 or 2
+            high--;
         }
     }
+}
 
     private static void swap(int[] arr, int i, int j){
         int temp = arr[i];
@@ -33,6 +36,7 @@ class DutchFlag{
             System.out.print(num + " ");
             System.out.println();
         }
+        System.out.println();
 
         int arr1[] = new int[] {2, 2, 0, 1, 2, 0 };
         DutchFlag.sort(arr1);
@@ -40,6 +44,5 @@ class DutchFlag{
             System.out.print(num + " ");
             System.out.println();
         }
-         
     }
 }
